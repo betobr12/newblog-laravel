86ac0289cd1592985844e71241349dcfc13cf5ba
unknown <betobr12@yahoo.com.br>
2020-04-13 14:06:15 -0300

    modificacoes

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,5 +26,16 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
+
+    public function __construct()
+    {
+        if (Auth::check() && Auth::user()->role->id == 1)
+        {
+            $this->redirectTo = route('admin.dashboard');
+        } else {
+            $this->redirectTo = route('author.dashboard');
+        }
+        $this->middleware('guest');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function
+(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function
+(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
