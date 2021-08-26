80dfc4d7fab53849bfee94f4576d3bc8713afc93
Roberto <betobr12@yahoo.com.br>
2021-08-26 09:14:21 -0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,16 +16,16 @@ class UsersTableSeeder extends Seeder
             'role_id' => '1',
             'name' => 'Beto.Admin',
             'username' => 'admin',
-            'email' => 'betobr08@gmail.com',
-            'password' => bcrypt('ramones123'),
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
         ]);
 
         DB::table('users')->insert([
             'role_id' => '2',
             'name' => 'Beto.Author',
             'username' => 'author',
-            'email' => 'betobr12@yahoo.com.br',
-            'password' => bcrypt('ramones123'),
+            'email' => 'author@yahoo.com.br',
+            'password' => bcrypt('admin'),
         ]);
     }
 }
